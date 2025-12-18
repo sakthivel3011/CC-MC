9e199bc40cb9946ede70a1de9fb74a28ffcb8cd7
SAKTHIVEL <sakthii3011@gmail.com>
2025-12-18 09:25:00 +0530

    Update Gallery.jsx

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -30,38 +30,13 @@ const Gallery = () => {
       new URL('../assets/images/Gallery/A/19.png', import.meta.url).href,
     ],
     '2k23': [
-      new URL('../assets/images/Gallery/A/1.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/3.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/5.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/7.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/9.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/11.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/13.png', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/15.png', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/17.png', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/19.png', import.meta.url).href,
+     
     ],
     '2k20': [
-      new URL('../assets/images/Gallery/A/2.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/4.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/6.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/8.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/10.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/12.png', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/14.png', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/16.png', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/18.png', import.meta.url).href,
+     
     ],
     '2k19': [
-      new URL('../assets/images/Gallery/A/5.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/7.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/9.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/11.JPG', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/13.png', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/15.png', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/17.png', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/19.png', import.meta.url).href,
-      new URL('../assets/images/Gallery/A/3.JPG', import.meta.url).href,
+     
     ],
     '2k18': [
       new URL('../assets/images/Gallery/A/1.JPG', import.meta.url).href,
